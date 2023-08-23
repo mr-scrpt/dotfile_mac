6bc2bbf0fdafec28ef62b392373672d2853b3658
mr.scrpt <mr.scrpt@gmail.com>
2023-08-23 12:21:36 +0300

    vim edited

--- nvim/lua/plugin/null-ls.lua
+++ nvim/lua/plugin/null-ls.lua
@@ -14,7 +14,7 @@ null_ls.setup({
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
-            vim.api.nvim_create_autocmd("BufWritePre", {
+            vim.api.nvim_create_autocmd("BufWrtePre", {
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
