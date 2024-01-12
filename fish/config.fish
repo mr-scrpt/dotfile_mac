84a23af435acaa2209be1915447a03756852201f
mr.scrpt <mr.scrpt@gmail.com>
2024-01-12 21:13:46 +0200

    lf fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,53 +1,48 @@
-# if status is-interactive
+if status is-interactive
     # Commands to run in interactive sessions can go here
-# end
 
-# ~/.config/fish/config.fish
+  neofetch
+  starship init fish | source
+  zoxide init fish | source
 
-neofetch
-starship init fish | source
-zoxide init fish | source
-set -gx fish_greeting # disable fish greeting
-set -U BAT_THEME Nord # 'sharkdp/bat' cat clone
-set -U EDITOR nvim # 'neovim/neovim' text editor
+  # restore fzf default options ('fzf clear')
+  # alias fzfcl='set -U FZF_DEFAULT_COMMAND "fd ."'
 
+# reinstate fzf custom options ('fzf-' as in 'cd -' as in 'back to where I was')
+  # alias fzf='set -U FZF_DEFAULT_COMMAND "fd . \$HOME"'
 
-set -U FZF_CTRL_R_OPTS "--border-label=' History ' --prompt=' '"
-set -U FZF_DEFAULT_COMMAND "fd -H -E '.git'"
+  set -x FZF_CTRL_R_OPTS "--border-label=' History ' --prompt=' '"
+  # set -U FZF_DEFAULT_COMMAND "fd -H -E '.git'"
+  set -x FZF_DEFAULT_COMMAND "fd . \$HOME -H -E '.git'"
 
-set -U FZF_DEFAULT_OPTS "--reverse --no-info --prompt=' ' --pointer='' --marker=' ' --ansi --color gutter:-1,bg+:-1,header:4,separator:0,info:0,label:4,border:4,prompt:7,pointer:5,query:7,prompt:7"
-set -U FZF_TMUX_OPTS "-p --no-info --ansi --color gutter:-1,bg+:-1,header:4,separator:0,info:0,label:4,border:4,prompt:7,pointer:5,query:7,prompt:7"
-
-
-
-set -U PAGER ~/.local/bin/nvimpager # 'lucc/nvimpager'
-set -U VISUAL nvim
+  set -x FZF_DEFAULT_OPTS "--reverse --no-info --prompt=' ' --pointer='' --marker=' ' --ansi --color gutter:-1,bg+:-1,header:4,separator:0,info:0,label:4,border:4,prompt:7,pointer:5,query:7,prompt:7"
+  set -x FZF_TMUX_OPTS "-p --no-info --ansi --color gutter:-1,bg+:-1,header:4,separator:0,info:0,label:4,border:4,prompt:7,pointer:5,query:7,prompt:7"
 
 
+  set -gx fish_greeting # disable fish greeting
+  set -U BAT_THEME Nord # 'sharkdp/bat' cat clone
+  set -U EDITOR nvim # 'neovim/neovim' text editor
 
-# aliases
-if type -q exa
-  alias ll "exa -l -g --icons"
-  alias lla "ll -a"
+  #FZF
+
+  #PATH
+  set -gx PATH bin $PATH
+  set -gx PATH ~/bin $PATH
+  set -gx PATH ~/.local/bin $PATH
+  set -gx PATH ~/.config/tmux/bin $PATH
+
+  # ordered by priority - bottom up
+  fish_add_path /opt/homebrew/bin # https://brew.sh/
+  fish_add_path /opt/homebrew/sbin
+  fish_add_path $HOME/.local/bin
+  fish_add_path $HOME/.local/share/nvim/site/pack
+
+  # aliases
+  if type -q exa
+    alias ll "exa -l -g --icons"
+    alias lla "ll -a"
+  end
+
+  command -qv nvim && alias v nvim
 end
-alias g git
-command -qv nvim && alias v nvim
 
-set -gx EDITOR nvim
-
-set -gx PATH bin $PATH
-set -gx PATH ~/bin $PATH
-set -gx PATH ~/.local/bin $PATH
-set -gx PATH ~/.config/tmux/bin $PATH
-set -gx PATH ~/.config/nvim/user/bash/ $PATH
-
-
-# ordered by priority - bottom up
-fish_add_path /opt/homebrew/bin # https://brew.sh/
-fish_add_path /opt/homebrew/sbin
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.local/share/nvim/site/pack
-
-# Setting PATH for Python 3.11
-# The original version is saved in /Users/mr/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
